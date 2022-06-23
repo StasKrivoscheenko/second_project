c156243a6dc57b200057ae20088bfedbb35d2fc2
stanislaw <staskrvoi.i@gmail.com>
2022-06-23 16:03:20 +0300

    adaptive +

--- JS/owl.carousel.js
+++ JS/owl.carousel.js
@@ -3446,22 +3446,4 @@
 	}
 
 })(window.Zepto || window.jQuery, window, document);
-$(document).ready(function(){
-	$(".owl-carousel").owlCarousel();
-});
-$$('.owl-carousel').owlCarousel({
-	loop:true,
-	margin:10,
-	nav:true,
-	responsive:{
-		0:{
-			items:1
-		},
-		600:{
-			items:3
-		},
-		1000:{
-			items:5
-		}
-	}
-})
+
